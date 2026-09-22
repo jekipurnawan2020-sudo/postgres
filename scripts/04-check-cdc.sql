31196d147ed4a526c189bcf40303bd1f9d20680e
unknown <yahyamatoristmik@gmail.com>
2026-09-22 18:05:52 +0700

    update

--- scripts/04-check-cdc.sql
+++ scripts/04-check-cdc.sql
@@ -1,3 +1,8 @@
-SELECT source_schema, source_table, capture_instance, start_lsn, supports_net_changes
+SELECT
+    OBJECT_SCHEMA_NAME(source_object_id) AS source_schema,
+    OBJECT_NAME(source_object_id) AS source_table,
+    capture_instance,
+    start_lsn,
+    supports_net_changes
 FROM cdc.change_tables
-ORDER BY source_schema, source_table;
+ORDER BY OBJECT_SCHEMA_NAME(source_object_id), OBJECT_NAME(source_object_id);
